daaec280e3f724838095271c6e94dc7c77c2c4aa
r-svp26 <riteshk6341@gmail.com>
2021-08-05 15:58:38 +0530

    [Ritesh] Add: Insert element at specific position into linkedlist

--- List/Program.cs
+++ List/Program.cs
@@ -4,12 +4,18 @@ namespace List
 {
     class Program
     {
+        /// <summary>
+        /// entry point of the application.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             LinkedList linkedList = new LinkedList();
             linkedList.Add(56);
-            linkedList.Add(30);
             linkedList.Add(70);
+            linkedList.Display();
+            Console.WriteLine("-----------------");
+            linkedList.InsertAtParticularPosition(1,30);
             linkedList.Display();
         }
     }
