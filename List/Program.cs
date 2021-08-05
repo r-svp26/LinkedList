5a40eede8b4612de8126b55d1f84b43ded2bf7c8
r-svp26 <riteshk6341@gmail.com>
2021-08-05 16:21:54 +0530

    [Ritesh] Add: Remove first element from linkedlist

--- List/Program.cs
+++ List/Program.cs
@@ -12,10 +12,11 @@ namespace List
         {
             LinkedList linkedList = new LinkedList();
             linkedList.Add(56);
+            linkedList.Add(30);
             linkedList.Add(70);
             linkedList.Display();
-            Console.WriteLine("-----------------");
-            linkedList.InsertAtParticularPosition(1,30);
+            Console.WriteLine("----------------");
+            linkedList.Pop();
             linkedList.Display();
         }
     }
