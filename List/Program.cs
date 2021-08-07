c50f4e8f7bf7037e81fb57509e0de299839f71e9
r-svp26 <riteshk6341@gmail.com>
2021-08-07 06:06:50 +0530

    [Ritesh] Add: Ordered LinkedList is implemented

--- List/Program.cs
+++ List/Program.cs
@@ -4,22 +4,50 @@ namespace List
 {
     class Program
     {
+        static readonly LinkedList linkedList = new LinkedList();
+        static readonly SortedLinkedList<int> sortedLinkedList = new SortedLinkedList<int>();
+        /// <summary>
+        /// view the linked list operations.
+        /// </summary>
+        public static void LinkedListView()
+        {
+            Console.WriteLine("<-------LinkedList------->");
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            int value = linkedList.Search(30);
+            linkedList.InsertAtParticularPosition(value + 1, 40);
+            linkedList.Display();
+            int foundKey = linkedList.Search(40);
+            linkedList.DeleteAtParticularPosition(foundKey);
+            linkedList.Size();
+            Console.WriteLine("<------------------------>\n");
+        }
+        /// <summary>
+        /// view the sorted operations.
+        /// </summary>
+        public static void SortedLinkedListView()
+        {
+            Console.WriteLine("<----SortedLinkedList---->");
+            sortedLinkedList.Add(new SortedNode<int>(70));
+            sortedLinkedList.Add(new SortedNode<int>(40));
+            sortedLinkedList.Add(new SortedNode<int>(30));
+            sortedLinkedList.Add(new SortedNode<int>(56));
+            Console.WriteLine("LinkedList before sorting");
+            sortedLinkedList.DisplaySort();
+            sortedLinkedList.SortOrderedList();
+            Console.WriteLine("LinkedList after sorting");
+            sortedLinkedList.DisplaySort();
+            Console.WriteLine("<------------------------>");
+        }
         /// <summary>
         /// entry point of the application.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            LinkedList linkedList = new LinkedList();
-            linkedList.Add(56);
-            linkedList.Add(30);
-            linkedList.Add(70);
-            int value = linkedList.Search(30);
-            linkedList.InsertAtParticularPosition(value + 1,40);
-            linkedList.Display();
-            int foundKey = linkedList.Search(40);
-            linkedList.DeleteAtParticularPosition(foundKey);
-            linkedList.Size();
+            Program.LinkedListView();
+            Program.SortedLinkedListView();
         }
     }
 }
